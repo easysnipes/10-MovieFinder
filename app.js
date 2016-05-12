2c5628144f929992bb65f34eef4b4ba1f4f06c3c
Stephen Boykin <Stephen@Stephens-MacBook.local>
2016-05-12 13:19:52 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+ var movies = angular.module('ImdbMovies', ['ionic']);
+
+ movies.controller('MovieControler', funciton($scope, $http, Movies)
+ {
+    $scope.searchmovie = 'no';
+    $scope.showloader = "no";
+    $scope.search = function()
+ {
+        $scope.showleader = "yes";
+        //console.log('clicked');
+        Movies.get(document.getElementById('movietitle').value)
+            .success(function(data) {
+                console.log(data);
+                
+                $scope.searchmovie ='yes';
+
+                $scope.title =data['Title'];
+                $scope.year = data['Year'];
+                $scope.runtime = data['Runtime'];
+                $scope.genre = data['Genre'];
+                $scope.imdbRating = data['imdbRating'];
+                $scope.director = data['Director'];
+                $scope.actors = data['Actors'];
+                $scope.language = data['Language'];
+                $scope.country = data['Country'];
+                $scope.awards = data['Awards'];
+                $scope.writer = data['Writer'];
+                $scope.plot = data['Plot'];
+                $scope.poster = data['Poster'];
+                $scope.response = data['Response'];
+                $scope.votes = data['imdbVotes'];
+
+                if(data['Response'] == 'False')
+                {
+                        $scope.error = data['Error'];
+                }
+
+                $scope.showloader = "no"
+
+
+                 });
+            }
+    });
+
+
+    movies.factory('Movies', function($http) {
+        return {
+            get: function(title) {
+                return $http({
+                    url: "http://www.omdbapi.com?t="+title+"&y=&plot=full&r=json",
+                method: "GET"
+                });
+            }
+        }
+
+    });
+
+
